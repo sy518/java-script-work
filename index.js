dbc5cc163913c0b9832dd4132799135969889fd1
Syeda Samana Zehra <174864609+sy518@users.noreply.github.com>
2024-09-19 00:52:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//   Q no 1
+alert(" Salam, My Name is Syeda Samana Zehra ");
+
+//    Q no 2
+var MyName = " Syeda Samana Zehra"
+alert(MyName);
+
+//    Q no 3
+var mySelf = "Salam, I am a Frontend Web Developer. "
+alert(mySelf + "My Name is Syeda Samana Zehra");
+console.log(mySelf + "My Name is Syeda Samana Zehra");
+
+
+//  Q no 4
+
+var a = 4
+var b = 2
+var answer = (a++ + --b - --a + b++ + a + b);
+console.log(answer)
+
+// (4 + 1 - 4 + 1 + 4 + 2) = 8
+
+//  Q no 5    
+
+var normalSuger = 120;
+var normalSuger = prompt("What is your current blood sugar level?");
+if (normalSuger >= 130) {
+    alert("You are a diabetic patient");
+    console.log("You are a diabetic patient");
+}
+else {
+    alert("Your health is Good");
+    console.log("Your health is Good");
+}
+
